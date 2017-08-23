3f9f249e87d311fd12b9760221b94b2206e21890
Milano Gadelha Carvalho <milanogc@gmail.com>
2017-08-22 21:58:19 -0300

    training and test set

--- kaggle/titanic/titanic.py
+++ kaggle/titanic/titanic.py
@@ -17,8 +17,8 @@ from sklearn.pipeline import Pipeline
 from sklearn_pandas import DataFrameMapper
 from sklearn_pandas import CategoricalImputer
 
-train_set = pd.read_csv(os.path.join("kaggle", "titanic", "train.csv"), index_col = "PassengerId")
-test_set = pd.read_csv(os.path.join("kaggle", "titanic", "test.csv"), index_col = "PassengerId")
+train_set = pd.read_csv("train.csv", index_col = "PassengerId")
+test_set = pd.read_csv("test.csv", index_col = "PassengerId")
 
 mapper = DataFrameMapper([
     ("Age", None),
@@ -30,15 +30,15 @@ mapper = DataFrameMapper([
 ], df_out = False)
 
 pipeline = Pipeline([
-    ('mapper', mapper),
-    ('imputer', Imputer()),
-    ('scaler', StandardScaler()),
-    #('classifier', SGDClassifier(random_state = 42, n_jobs = 4)), # Stochastic Gradient Descent
-    ('classifier', RandomForestClassifier(random_state = 42, n_jobs = 4))
+    ("mapper", mapper),
+    ("imputer", Imputer()),
+    ("scaler", StandardScaler()),
+    #("classifier", SGDClassifier(random_state = 42, n_jobs = 4)), # Stochastic Gradient Descent
+    ("classifier", RandomForestClassifier(random_state = 42, n_jobs = 4))
 ])
 
 train_set_labels = train_set["Survived"]
 pipeline.fit(train_set, train_set_labels)
 test_set["Survived"] = pipeline.predict(test_set)
-test_set[["Survived"]].to_csv(os.path.join("kaggle", "titanic", "result.csv"))
+test_set[["Survived"]].to_csv("result.csv")
 
